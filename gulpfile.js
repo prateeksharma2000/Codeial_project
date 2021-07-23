1abd9678b42dd8b7ffa9663319172ad9ca150a49
Prateek <ps1077211@gmail.com>
2021-07-23 21:36:24 +0530

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
 const uglify = require('gulp-uglify-es').default;
-const imagemin = require('gulp-imagemin');
 const del = require('del');
 
 gulp.task('scss_to_css', function (done)
@@ -53,38 +52,16 @@ gulp.task('js', function (done)
     done();
 })
 
-
-
-gulp.task('images', function (done)
-{
-    console.log('Compressing Images');
-
-    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-        .pipe(imagemin())
-        .pipe(rev())
-        .pipe(gulp.dest('./public/assets'))
-        .pipe(rev.manifest({
-            base: 'public',
-            merge: 'true'
-        }))
-        .pipe(gulp.dest('./public'));
-
-    done();
-});
-
-
-
 //this task will clear the public/assets directory
 gulp.task('clean:assets', function (done)
 {
     del.sync('./public/css');
     del.sync('./public/js');
-    del.sync('./public/images');
     done();
 });
 
 // main builder
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function (done)
+gulp.task('build', gulp.series('clean:assets', 'css', 'js'), function (done)
 {
     console.log('Building Assets');
     done();
