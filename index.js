3fbcdc8069d4290a397f1ef040f9f0f476803532
Prateek <ps1077211@gmail.com>
2021-07-25 13:39:02 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
 require('./config/view_helpers')(app);
-const port = 9000;
+const port =process.env.PORT || 9000;
 const routes = require('./routes/entry_point_router');
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
