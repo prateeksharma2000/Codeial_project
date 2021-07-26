e662ba01df42a2c4f024641432bb88ecd169b0f3
Prateek <ps1077211@gmail.com>
2021-07-26 17:26:31 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,9 @@ const env=require('./environment');
 const mongoose=require('mongoose');
 mongoose.connect(`mongodb://localhost/${env.database_name}`, { useNewUrlParser: true,useUnifiedTopology: true });
 const db=mongoose.connection;
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 db.on('error', console.error.bind(console, 'Error in connecting to the database'));
 
