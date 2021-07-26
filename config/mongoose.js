1283351f936727c365d283659858df0879acffc3
Prateek <ps1077211@gmail.com>
2021-07-26 18:16:30 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const env=require('./environment');
 const mongoose=require('mongoose');
-mongoose.connect(`mongodb://localhost/${env.database_name}`, { useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://prateek:prateek2000@cluster0.ixwxz.mongodb.net/codeial?retryWrites=true&w=majority`, { useNewUrlParser: true,useUnifiedTopology: true });
 const db=mongoose.connection;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
